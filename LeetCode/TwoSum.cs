ab264dcf6e94c3d25015bf1abc565c54e3b00e94
ryan-way <ryan.keith.way@gmail.com>
2018-11-20 22:42:00 -0800

    Added Two Sum solution (previously solved)

---
+++ LeetCode/TwoSum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TwoSum
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] retVal = new int[2];
+
+            Dictionary<int, int> foundValues = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int value = nums[i];
+                if (foundValues.ContainsKey(target - value))
+                {
+                    retVal[0] = foundValues[target - value];
+                    retVal[1] = i;
+                    return retVal;
+                }
+                else if (!foundValues.ContainsKey(value))
+                {
+                    foundValues.Add(value, i);
+                }
+            }
+
+            return new int[] { };
+        }
+    }
+}
