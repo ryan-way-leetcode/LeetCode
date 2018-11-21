488ba010a787a6bdceec352e9eaf17ffe49e714a
ryan-way <ryan.keith.way@gmail.com>
2018-11-20 22:46:09 -0800

    Add solution for longest subsequence without repeating characters (previously solved)

---
+++ LeetCode/LongestSubsequenceWithoutRepeats.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LongestSubsequenceWithoutRepeats
+{
+    public class Solution
+    {
+        public const int NO_OF_CHARS = 256;
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (s.Length == 0) return 0;
+            int max = 1;
+            int[] visited = new int[256];
+            for (int i = 0; i < visited.Length; i++)
+                visited[i] = -1;
+
+            int cur = 0;
+            int start = 0;
+            visited[s[cur]] = cur++;
+
+            for (; cur < s.Length; cur++)
+            {
+                if (visited[s[cur]] >= start)
+                {
+                    start = visited[s[cur]] + 1;
+                    if (max > (s.Length - cur + 1)) return max;
+                }
+                visited[s[cur]] = cur;
+                max = Math.Max(max, cur - start + 1);
+            }
+
+            return max;
+        }
+    }
+}
