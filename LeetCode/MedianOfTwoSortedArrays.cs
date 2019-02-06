b25d8b8bbd6c3af8c70d4818ebd18fe25116bc6f
Ryan Way <ryan.keith.way@gmail.com>
2019-02-06 13:55:08 -0800

    starts median of two sorted arrays

---
+++ LeetCode/MedianOfTwoSortedArrays.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MedianOfTwoSortedArrays
+{
+    public class Solution
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            int i_min = 0, i_max = nums1.Length - 1;
+            int i = (i_min + i_max) / 2;
+            int j = (nums1.Length + nums2.Length + 1) / 2 - i - 2;
+
+            if(nums2.Length == 0)
+            {
+                if (nums1.Length % 2 == 0) return (nums1[i] + nums1[i + 1]) / 2;
+                else return nums1[i];
+            }
+            else if(nums1.Length == 0)
+            {
+                if (nums2.Length % 2 == 0) return (nums2[i] + nums2[i + 1]) / 2;
+                else return nums2[i];
+            }
+
+            while((j < nums2.Length - 1 && nums1[i] <= nums2[j+1]) && 
+                (i < nums1.Length - 1 && nums2[j] <= nums1[i+1]))
+            {
+                if (j < nums2.Length - 1 && nums1[i] > nums2[j + 1])
+                    i_max = i;
+                else if (j < nums2.Length - 1 && nums2[j] < nums1[i + 1])
+                    i_min = i;
+
+
+                i = (i_min + i_max) / 2;
+                j = (nums1.Length + nums2.Length + 1) / 2 - i - 2;
+            }
+
+            if ((nums1.Length + nums2.Length) % 2 == 0)
+            {
+                if(i < nums1.Length && j < nums2.Length)
+                    return (nums1[i] + nums2[j]) / 2.0;
+            }
+            else
+            {
+                if (i < nums1.Length && j < nums2.Length)
+                    return Math.Max(nums1[i], nums2[j]);
+                else if (i >= nums1.Length && j < nums2.Length)
+                    return nums2[j];
+                else if (j >= nums2.Length && i < nums1.Length)
+                    return nums1[i];
+                else return 0;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ LeetCodeTest/MedianOfTwoSortedArraysTest.cs
@@ -0,0 +1,51 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MedianOfTwoSortedArrays;
+
+namespace LeetCodeTest
+{
+    [TestClass]
+    public class MedianOfTwoSortedArraysTest
+    {
+        public Solution Solution;
+
+        [TestInitialize]
+        public void Initialization()
+        {
+            Solution = new Solution();
+        }
+
+        [TestMethod]
+        public void Example1()
+        {
+            double actual = Solution.FindMedianSortedArrays(new int[] { 1, 3 }, new int[] { 2 });
+            Assert.AreEqual(2.0, actual);
+            actual = Solution.FindMedianSortedArrays(new int[] { 2 }, new int[] { 1, 3 });
+            Assert.AreEqual(2.0, actual);
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            double actual = Solution.FindMedianSortedArrays(new int[] { 1, 2 }, new int[] { 3, 4 });
+            Assert.AreEqual(2.0, actual);
+            actual = Solution.FindMedianSortedArrays(new int[] { 3, 4 }, new int[] { 1, 2 });
+            Assert.AreEqual(2.0, actual);
+        }
+
+        [TestMethod]
+        public void Failed1()
+        {
+            double actual = Solution.FindMedianSortedArrays(new int[] { }, new int[] { 1 });
+            Assert.AreEqual(1.0, actual);
+            actual = Solution.FindMedianSortedArrays(new int[] { 1 }, new int[] { });
+            Assert.AreEqual(1.0, actual);
+        }
+
+        [TestMethod]
+        public void Failed2()
+        {
+            double actual = Solution.FindMedianSortedArrays(new int[] { 1 }, new int[] { 1 });
+            Assert.AreEqual(1.0, actual);
+        }
+    }
+}
