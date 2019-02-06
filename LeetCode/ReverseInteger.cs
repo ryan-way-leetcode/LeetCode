e8bf009221223eae16b8202f26e4b0d1e8260b27
Ryan Way <ryan.keith.way@gmail.com>
2019-02-06 14:45:36 -0800

    Reverse integer

---
+++ LeetCode/ReverseInteger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReverseInteger
+{
+    public class Solution
+    {
+        public int Reverse(int x)
+        {
+            long input = x;
+            //long to list
+            bool negative = x < 0;
+            input = Math.Abs(input);
+            long divide = 1;
+            List<long> list = new List<long>();
+            while(divide <= input)
+            {
+                divide *= 10;
+                list.Add((input % divide)/(divide/10));
+            }
+            
+            //list to reversed long
+            long y = 0;
+            divide = 1;
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                y += list[i] * divide;
+                divide *= 10;
+            }
+
+            //overflow handling
+            if (y > int.MaxValue || y < int.MinValue) return 0;
+
+            //return
+            return (int)(negative? -y : y);
+        }
+    }
+}
---
+++ LeetCodeTest/ReverseIntegerTest.cs
@@ -0,0 +1,53 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReverseInteger;
+
+namespace LeetCodeTest
+{
+    [TestClass]
+    public class ReverseIntegerTest
+    {
+        public Solution Solution;
+
+        [TestInitialize]
+        public void Initialization()
+        {
+            Solution = new Solution();
+        }
+
+        [TestMethod]
+        public void Example1()
+        {
+            Assert.AreEqual(Solution.Reverse(123), 321);
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            Assert.AreEqual(Solution.Reverse(-123), -321);
+        }
+
+        [TestMethod]
+        public void Example3()
+        {
+            Assert.AreEqual(Solution.Reverse(120), 21);
+        }
+
+        [TestMethod]
+        public void Failed1()
+        {
+            Assert.AreEqual(Solution.Reverse(-2147483412), -2143847412);
+        }
+
+        [TestMethod]
+        public void Failed2()
+        {
+            Assert.AreEqual(Solution.Reverse(-2147483648), 0);
+        }
+
+        [TestMethod]
+        public void Failed3()
+        {
+            Assert.AreEqual(Solution.Reverse(1), 1);
+        }
+    }
+}
