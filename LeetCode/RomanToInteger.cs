6ab9e50d6c886cf836c78c50d0012522cf390c85
Ryan Way <ryan.keith.way@gmail.com>
2019-02-06 15:22:30 -0800

    start roman to integer

---
+++ LeetCode/RomanToInteger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RomanToInteger
+{
+    public class Solution
+    {
+        public int RomanToInt(string s)
+        {
+            return 0;
+        }
+    }
+}
---
+++ LeetCodeTest/RomanToIntegerTest.cs
@@ -0,0 +1,47 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RomanToInteger;
+
+namespace LeetCodeTest
+{
+    [TestClass]
+    public class RomanToIntegerTest
+    {
+        public Solution Solution;
+
+        [TestInitialize]
+        public void Initialization()
+        {
+            Solution = new Solution();
+        }
+
+        [TestMethod]
+        public void Example1()
+        {
+            Assert.AreEqual(Solution.RomanToInt("III"), 3);
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            Assert.AreEqual(Solution.RomanToInt("IV"), 4);
+        }
+
+        [TestMethod]
+        public void Example3()
+        {
+            Assert.AreEqual(Solution.RomanToInt("IX"), 9);
+        }
+
+        [TestMethod]
+        public void Example4()
+        {
+            Assert.AreEqual(Solution.RomanToInt("LVIII"), 58);
+        }
+
+        [TestMethod]
+        public void Example5()
+        {
+            Assert.AreEqual(Solution.RomanToInt("MCMXCIV"), 1994);
+        }
+    }
+}
